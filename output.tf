8193dd3f0ff79e0f3ec6232abde588edb9f35fc0
Ghostechos <piyas.barua@proton.me>
2025-10-31 13:57:08 +0100

    Add outputs for VM ID and VM name in output.tf

---
+++ output.tf
@@ -0,0 +1,14 @@
+root@debian:/opt/flask-clone/tofu/proxmox# cat outputs.tf 
+output "vm_id" {
+  value = proxmox_vm_qemu.debian_clone.vmid
+}
+
+# Supprime ou commente le bloc ci-dessous :
+# output "ip_address" {
+#   value = proxmox_vm_qemu.debian_clone.network.0.ip
+# }
+
+# Optionnel : affiche juste le nom et l’état de la VM
+output "vm_name" {
+  value = proxmox_vm_qemu.debian_clone.name
+}
